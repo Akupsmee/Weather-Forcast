256b0507d0e71c8909cb81cf10d591cda79e2908
Akupsmee <akupuomeikenna@gmail.com>
2021-06-02 17:15:28 +0200

    my weather app initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const https = require("https");
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const app = express();
+const port = 4000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let weather = {};
+let query = "Bremen";
+const client = new Client({});
+
+app.get("/", (req, res) => {
+  res.render("index", { weather: weather, query: query, client });
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+    
+
+  const id = process.env.APP_ID;
+  const unit = "metric";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${id}&units=${unit}`;
+
+  https.get(url, (response) => {
+    response.on("data", (data) => {
+      const d = JSON.parse(data);
+      console.log(d);
+      weather = {
+        temperature: d.main.temp,
+        description: d.weather[0].description,
+        icon: d.weather[0].icon,
+        lon: d.coord.lon,
+        lat: d.coord.lat,
+        feels: d.main.feels_like,
+      };
+
+   
+      client
+        .elevation({
+          params: {
+            locations: [{ lat: weather.lat, lng: weather.lon }],
+            key: process.env.API_KEY,
+          },
+          timeout: 1000, // milliseconds
+        })
+        .then((r) => {
+          console.log(r.data.results[0].elevation);
+        })
+        .catch((e) => {
+          console.log(e.response.data.error_message);
+        });
+
+      console.log(weather.lon);
+      console.log(weather.lat);
+
+      res.render("index", { weather, query });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log("app listening on port 4000");
+});
+
